3646e58966dd5506acbeb3f8b6fd37cfd76c5181
toddmurphy <toddmurphy93@gmail.com>
2020-01-29 12:35:49 -0400

    working on getting jwt token working

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,4 +1,6 @@
 const router = require('express').Router();
+const jwt = require('jsonwebtoken');
+const secrets = require('../config/secrets');
 const bcrypt = require('bcryptjs');
 
 const Users = require('../users/users-model.js');
@@ -25,8 +27,11 @@ router.post('/login', (req, res) => {
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        const token = generateToken(user);
+
+        // the server needs to return the token to the client
         res.status(200).json({
-          message: `Welcome ${user.username}!`,
+          message: `Welcome ${user.username}!, we have a token...`
         });
       } else {
         res.status(401).json({ message: 'Invalid Credentials' });
@@ -36,5 +41,21 @@ router.post('/login', (req, res) => {
       res.status(500).json(error);
     });
 });
+
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+    //add any other data we want to store in token payload
+  };
+
+  const options = {
+    expiresIn: '1d' //check documentation for other options available
+  };
+
+  // extract the secret away so it can be required and used where needed
+
+  return jwt.sign(payload, secrets.jwtSecret, options);
+}
 
 module.exports = router;
